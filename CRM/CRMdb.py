fc3b05e8d43f99e352ac2810f4257eacf5e3eec5
Taha Shaikh <tahashaikh20082006@gmail.com>
2025-07-01 16:34:13 +0530

    Initial commit

---
+++ CRM/CRMdb.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '20082006'
+
+)
+
+cursorObject = database.cursor()
+
+cursorObject.execute("CREATE DATABASE CRMdb")
