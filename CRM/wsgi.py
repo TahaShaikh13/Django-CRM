4e7a89a3b9cea554fde320909337884e7ea02811
Taha Shaikh <tahashaikh20082006@gmail.com>
2025-08-07 20:42:54 +0530

    Resolving bug

--- CRM/wsgi.py
+++ CRM/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRM.settings')
 
 application = get_wsgi_application()
+
+if os.environ.get('RUN_MAIN') != 'true':
+    call_command('migrate')
+    call_command('collectstatic',interactive=False, verbosity=0)
