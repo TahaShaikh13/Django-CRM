5aa58ed5dd6ba875dce708e2c32d71ceb792e4e0
Taha Shaikh <tahashaikh20082006@gmail.com>
2025-08-12 18:32:22 +0530

    -

--- create_superuser.py
+++ create_superuser.py
@@ -1,14 +1,20 @@
-from django.contrib.auth import get_user_model
+# create_superuser_view.py
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRM.settings')  # Change CRM to your project name
+django.setup()
+
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 username = "tahashaikh13"
 email = "tahashaikh20082006@gmail.com"
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "taha20082006")
+password = "taha20082006"
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
-    print(f"Superuser '{username}' created.")
+    print("Superuser created successfully!")
 else:
-    print(f"Superuser '{username}' already exists.")
+    print("Superuser already exists.")
